f29178493af0b4444f804b992cb678972a4f2f61
robin <unknown>
2021-08-26 10:21:57 +0100

    make test command more explicit

--- test/runAll.py
+++ test/runAll.py
@@ -1,11 +1,20 @@
-#special case import to allow reportlab to modify the envirnment in development
-try:
-    import reportlab
-except ImportError:
-    pass
+def main():
+    import sys, os
+    wd = os.path.dirname(os.path.abspath(sys.argv[0]))
+    sys.path.insert(0,wd)
+    os.chdir(wd)
+    
+    #special case import to allow reportlab to modify the envirnment in development
+    try:
+        import reportlab
+    except ImportError:
+        pass
 
-import leaktest, testRXPbasic, test_xmltestsuite
+    import leaktest, testRXPbasic, test_xmltestsuite
 
-leaktest.main(100)
-testRXPbasic.main()
-test_xmltestsuite.main()
+    leaktest.main(100)
+    testRXPbasic.main()
+    test_xmltestsuite.main()
+
+if __name__=='__main__':
+    main()
